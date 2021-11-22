5705938d14232804b913f615efb2cb62ef5b9c1f
Ryo Ishizawa <ryoishizawa298142@gmail.com>
2021-11-22 22:40:45 +0800

    implement tokenizer (WIP)

--- 9cc.c
+++ 9cc.c
@@ -1,5 +1,40 @@
+#include <ctype.h>
+#include <stdarg.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+typedef enum {
+  TK_RESERVED,
+  TK_NUM,
+  TK_EOF,
+} TokenKind;
+
+typedef struct Token Token;
+
+struct Token {
+  TokenKind kind;
+  Token *next;
+  int val;
+  char *str;
+};
+
+Token *token;
+
+void error(char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+  vfprintf(stderr, fmt, ap);
+  fprintf(stderr, "\n");
+  exit(1);
+}
+
+bool consume(char op) {
+  if (token -> kind != TK_RESERVED || token -> str[0] != op)
+    error("It is not '%c'", op);
+    token = token -> next;
+}
 
 int main(int argc, char **argv) {
   if (argc != 2) {
